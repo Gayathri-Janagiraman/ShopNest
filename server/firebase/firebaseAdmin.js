fd58bebc28595dc0731f7ac1705efa13d94db89f
Gayathri-Janagiraman <gayathrijanagiraman86@gmail.com>
2025-06-09 13:05:20 +0530

    Updated

--- server/firebase/firebaseAdmin.js
+++ server/firebase/firebaseAdmin.js
@@ -2,6 +2,8 @@ const { initializeApp, cert } = require("firebase-admin/app");
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY);
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 initializeApp({
   credential: cert(serviceAccount),
 });
